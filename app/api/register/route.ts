f07532c011fdfd2625ff57b16a9cf8fc904ded2a
Running water <alsgks1218@naver.com>
2023-06-18 16:52:24 +0900

    validateAsync를 쓴다면 e.name과 e.message를 담아서 보내서 처리할 수도 있음

--- app/api/register/route.ts
+++ app/api/register/route.ts
@@ -1,16 +1,25 @@
 import { NextResponse } from "next/server";
 import prisma from "@/app/libs/prismadb";
+import SignupSchema from "@/app/libs/validations/SignupSchema";
 
 export async function POST(request: Request) {
   try {
     const body = await request.json();
     console.log(body);
+    const result = await SignupSchema.validateAsync(body);
+    console.log(result);
     return NextResponse.json({
       statuss: 200,
       oks: true,
     });
-  } catch (e) {
-    console.log(e);
+  } catch (e: any) {
+    if (e.name === "ValidationError") {
+      return NextResponse.json({
+        statuss: 200,
+        oks: false,
+        message: e.message,
+      });
+    }
     return NextResponse.json({
       statuss: 200,
       oks: false,
